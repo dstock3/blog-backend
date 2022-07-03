c68706772dc29826f2929232787d7c5a6b39be86
David Stockdale <dstock3@gmail.com>
2022-07-03 17:19:49 -0500

    Installed mongoose and added connection string.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import models from './models';
 import routes from './routes';
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use((req, res, next) => {
   req.context = {
