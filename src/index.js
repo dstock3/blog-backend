c41577626977052682bce564264f9916c4624e8b
David Stockdale <dstock3@gmail.com>
2022-07-03 14:00:07 -0500

    installed curl and tested uris

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ const app = express();
 
 app.use(cors());
 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+  
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+  
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
 );
