cf6c3082ef39bedcd350d36d0a369f6e8a7d7adb
David Stockdale <dstock3@gmail.com>
2022-07-03 14:12:06 -0500

    Started added structure for routes.

--- src/index.js
+++ src/index.js
@@ -6,21 +6,71 @@ const app = express();
 
 app.use(cors());
 
+/* Get home page */
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
+
+// USERS
+
+/* Get user landing page */
+app.get('/:username', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+/* Get specific article */
+app.get('/:username/:articleId', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+/* Compose article */
+app.post('/compose', (req, res) => {
+    return res.send(
+        `POST HTTP method on /compose resource`,
+    );
+});
+
+/* Update specific article */
+app.put('/:username/:articleId', (req, res) => {
+    return res.send(
+        `PUT HTTP method on username/${req.params.articleId} resource`,
+    );
+});
+
+/* Delete specific article */
+app.delete('/:username/:articleId', (req, res) => {
+    return res.send(
+        `DELETE HTTP method on username/${req.params.articleId} resource`,
+    );
+});
+
+/* Get user profile */
+app.get('/:username/profile', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+/* Create user profile */
+app.post('/register', (req, res) => {
+    return res.send(
+        `POST HTTP method on user/${req.params.username} resource`,
+    );
+});
+
+/* Update user profile */
+app.put('/:username/profile', (req, res) => {
+    return res.send(
+        `PUT HTTP method on user/${req.params.username}/profile resource`,
+    );
+});
+
+/* Delete user profile */
+app.delete('/:username/profile', (req, res) => {
+    return res.send(
+        `DELETE HTTP method on user/${req.params.userId}/profile resource`,
+    );
+});
   
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
 
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-});
   
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
