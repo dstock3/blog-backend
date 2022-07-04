900655959dcfeb496d4efd44a3b8c08972bb4268
David Stockdale <dstock3@gmail.com>
2022-07-04 00:35:01 -0500

    Added properties to article schema.

--- src/models/users.js
+++ src/models/users.js
@@ -4,14 +4,16 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    profileName: {type: String, required: true, maxLength: 100},
+    profileName: {type: String, required: true, minLength: 5, maxLength: 100},
+    password: {type: String, required: true, minLength: 5 },
+    admin: {type: Boolean, default: false},
     profileDesc: {type: String, required: true, maxLength: 350},
     profilePic: {type: String},
     themePref: {type: String, required: true, default: "dark"},
     layoutPref: {type: String, required: true, default: "basic"},
     blogTitle: {type: String, required: true, maxLength: 100},
     dateJoined: {type: Date},
-    articles: {type: Array}
+    articles: [{type: Schema.Types.ObjectId, ref: 'Articles'}]
   }
 );
 
