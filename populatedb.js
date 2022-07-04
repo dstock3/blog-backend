ec502292f60e49a3e338231b94e28906bec56ef6
David Stockdale <dstock3@gmail.com>
2022-07-04 13:59:57 -0500

    Created arrow function to create article objects.

--- populatedb.js
+++ populatedb.js
@@ -10,7 +10,7 @@ if (!userArgs[0].startsWith('mongodb')) {
 */
 import async from 'async'
 import Article from './models/articles'
-import Users from './models/users'
+import User from './models/users'
 
 import mongoose from 'mongoose';
 const mongoDB = userArgs[0];
@@ -21,4 +21,25 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const articles = []
 const users = []
+
+const createArticles = (title, img, imgDesc, date, content, comments) => {
+    let article = new Article(title, img, imgDesc, date, content, comments);
+
+    article.save(function (err) {
+        if (err) { cb(err, null)
+          return
+        }
+        console.log('New Article: ' + article);
+        articles.push(article)
+        cb(null, article)
+    });
+
+}
+
+const createUsers = (profileName, password, admin, profileDesc, profilePic, themePref, layoutPref, blogTitle, dateJoined, articles) => {
+    let username = new User(userDetail);
+
+}
+
+
 
