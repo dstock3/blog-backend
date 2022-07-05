8235e3ff9581abc84424098947664c88bd3dc553
David Stockdale <dstock3@gmail.com>
2022-07-05 10:14:48 -0500

    Changed env variable for connection string.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.URI, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
