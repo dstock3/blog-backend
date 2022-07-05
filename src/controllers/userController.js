bd73aaf0b39684a3607217dfb2094bc6faf947a4
David Stockdale <dstock3@gmail.com>
2022-07-05 16:05:35 -0500

    Converted index controller to async function. Added update user controller.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,13 +3,16 @@ import Article from '../models/articles.js';
 import async from 'async';
 import { body, validationResult } from "express-validator";
 
-const index = function(req, res, next) {
-    User.find({}, 'profileName admin profileDesc profilePic themePref layoutPref blogTitle dateJoined articles')
-    .populate('articles')
-    .exec(function(err, results) {
-        if (err) { return next(err); }
-        res.send({users: results});
-    });
+const index = async function(req, res, next) {
+    try { let isLoggedIn = false;
+        if (res.locals.currentUser) { isLoggedIn = true };
+
+        const results = await User.find({}, 'profileName admin profileDesc profilePic themePref layoutPref blogTitle dateJoined articles')
+        .populate('articles');
+
+        res.send({users: results, isLoggedIn: isLoggedIn });
+
+    } catch(err) { return next(err) }
 };
 
 const login_post = function(req, res, next) {
@@ -20,8 +23,13 @@ const register_post = function(req, res, next) {
     console.log("register post")
 }
 
-const user_update = function(req, res, next) {
-    console.log("user update")
+const user_update = async function(req, res, next) {
+    const userToUpdate = await User.findOne({_id: res.locals.currentUser._id})
+
+    userToUpdate.save(err =>{
+      if (err) { return next(err) }
+      res.send("update successful")
+    })
 }
 
 const user_delete = function(req, res, next) {
