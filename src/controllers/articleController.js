ef7347ee9579c0d9c4833109c6fc3a3e352a1920
David Stockdale <dstock3@gmail.com>
2022-07-06 14:51:15 -0500

    included missing async keyword for article update controller.

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -25,6 +25,7 @@ const article_create_post = [
         content: req.body.content,
         blogTitle: req.body.blogTitle
       })
+      console.log("post article")
 
       article.save(err => {
         if (err) { return next(err) }
@@ -37,31 +38,31 @@ const article_create_post = [
   }
 ];
 
-const article_update_put = function(req, res, next) {
-    console.log("post update")
-    const postToUpdate = await Article.findOne({_id: req.req.body.articleId})
-  
-    postToUpdate.save(err =>{
-      if (err) { return next(err) }
-      res.send("update successful")
-    })
+const article_update_put = async function(req, res, next) {
+  console.log("post update")
+  const postToUpdate = await Article.findOne({_id: req.req.body.articleId})
 
+  postToUpdate.save(err =>{
+    if (err) { return next(err) }
+    res.send("update successful")
+  })
 }
 
 const article_delete_post = function(req, res, next) {
-    const articleId = req.body.articleId
+  console.log("post deleted")
+  const articleId = req.body.articleId
     
-    User.findOneAndUpdate(
-      {_id: req.body.userId},
-      {$pull: { articles: req.body.articleId }
-      }, function(err, id) {
+  User.findOneAndUpdate(
+    {_id: req.body.userId},
+    {$pull: { articles: req.body.articleId }
+    }, function(err, id) {
+      if (err) { return next(err) }
+      
+      Article.findByIdAndDelete(articleId, function(err, docs){
         if (err) { return next(err) }
-        
-        Article.findByIdAndDelete(articleId, function(err, docs){
-          if (err) { return next(err) }
-          res.send("article deleted");
-    })
-  })
+        res.send("article deleted");
+      })
+    })  
 }
 
 export default { article_create_post, article_update_put, article_delete_post }
