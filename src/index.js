8d3f9b12df904efff6a1bc21438fa6236bb5f4ba
David Stockdale <dstock3@gmail.com>
2022-07-11 16:15:55 -0500

    Employued morgan and cookieParser middleware.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import 'dotenv/config';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import compression from 'compression';
 import helmet from 'helmet';
 import routes from './routes/index.js'
-import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import './auth/passport.js'
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 mongoose.connect(process.env.URI, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(compression());
 
