1b415bb62a8fb4d5b8c40eb0504e421c9f4642b9
David Stockdale <dstock3@gmail.com>
2022-08-19 15:43:41 -0500

    Revised user model

--- src/models/users.js
+++ src/models/users.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
     password: {type: String, required: true, minLength: 5 },
     admin: {type: Boolean, default: false},
     profileDesc: {type: String, required: true, maxLength: 350},
-    profilePic: {type: String},
+    profilePic: {type: String, typeKey: '$type' },
     themePref: {type: String, default: "dark"},
     layoutPref: {type: String, default: "basic"},
     blogTitle: {type: String, required: true, maxLength: 100},
